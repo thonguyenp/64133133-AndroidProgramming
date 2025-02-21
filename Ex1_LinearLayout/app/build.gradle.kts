80bd2bcb0c48cb986e38354d9cca456078af03a0
thonguyenp <nphuoctho2406@gmail.com>
2025-02-21 18:18:39 +0700

    Thêm ảnh kết quả cho LinearLayout và ConstraintLayout

--- Ex1_LinearLayout/app/build.gradle.kts
+++ Ex1_LinearLayout/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.ex1_linearlayout"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.ex1_linearlayout"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
