43d9e17b858ccdb796536d9654267a0730576546
thonguyenp <nphuoctho2406@gmail.com>
2025-02-22 19:56:09 +0700

    Xong phần backend của cộng và trừ

--- Ex4_AddSubMulDiv_Onclick/app/src/main/java/com/example/ex4_addsubmuldiv_onclick/MainActivity.java
+++ Ex4_AddSubMulDiv_Onclick/app/src/main/java/com/example/ex4_addsubmuldiv_onclick/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex4_addsubmuldiv_onclick;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,33 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void XuLyCong(View view)
+    {
+        Button btnCong = findViewById(R.id.btnCong);
+        EditText editA = findViewById(R.id.editA);
+        EditText editB = findViewById(R.id.editB);
+        EditText editKQ = findViewById(R.id.editKQ);
+
+        int a = Integer.parseInt(editA.getText().toString());
+        int b = Integer.parseInt(editB.getText().toString());
+
+        int kq = a + b;
+        editKQ.setText(kq + "");
+    }
+
+    public void XuLyTru(View view)
+    {
+        Button btnTru = findViewById(R.id.btnTru);
+        EditText editA = findViewById(R.id.editA);
+        EditText editB = findViewById(R.id.editB);
+        EditText editKQ = findViewById(R.id.editKQ);
+
+        int a = Integer.parseInt(editA.getText().toString());
+        int b = Integer.parseInt(editB.getText().toString());
+
+        int kq = a - b;
+        editKQ.setText(kq + "");
     }
 }
