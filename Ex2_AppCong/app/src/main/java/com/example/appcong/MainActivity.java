26c88d486d32eecc13c5b0e50491fb71fe8ef9b8
thonguyenp <nphuoctho2406@gmail.com>
2025-02-22 14:36:00 +0700

    Hoàn thành phần backend bài ex2

--- Ex2_AppCong/app/src/main/java/com/example/appcong/MainActivity.java
+++ Ex2_AppCong/app/src/main/java/com/example/appcong/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.appcong;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    //Bộ lắng nghe và xử lí sự kiện click lên btn
+    public void XuLyCong(View view)
+    {
+        //mapping sang file java
+        EditText editA = findViewById(R.id.editA);
+        EditText editB = findViewById(R.id.editB);
+        EditText editKQ = findViewById(R.id.editKQ);
+
+        //lấy dữ liệu và lưu vào biến
+        String strA = editA.getText().toString();
+        String strB = editB.getText().toString();
+
+        //Chuyển dữ liệu
+        int a = Integer.parseInt(strA);
+        int b = Integer.parseInt(strB);
+        int kq = a + b;
+        editKQ.setText(kq + "");
     }
 }
