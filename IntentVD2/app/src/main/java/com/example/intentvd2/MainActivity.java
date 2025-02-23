573e9da21e155be89e9860a9f2c2cb30504c0c6a
thonguyenp <nphuoctho2406@gmail.com>
2025-02-23 19:25:05 +0700

    Xong phần frontend và backend bên main

---
+++ IntentVD2/app/src/main/java/com/example/intentvd2/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.intentvd2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void NhapLieu (View view)
+    {
+        Intent iNhap = new Intent(this, SubActivity.class);
+        startActivityForResult(iNhap, 8000);
+    }
+
+}
