f16c0ffa5c73556dd531f37a87e12a1521926597
thonguyenp <nphuoctho2406@gmail.com>
2025-02-24 12:04:49 +0700

    Hoàn thành phần backend và frontend của 2 activity còn lại

--- IntentLogin/app/src/main/java/com/example/intentlogin/ActivityHome.java
+++ IntentLogin/app/src/main/java/com/example/intentlogin/ActivityHome.java
@@ -1,9 +1,7 @@
 package com.example.intentlogin;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,25 +9,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button btnLogin;
+public class ActivityHome extends AppCompatActivity {
+    TextView txtTenDangNhap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        btnLogin = findViewById(R.id.btnLogin);
-    }
+        txtTenDangNhap = findViewById(R.id.txtTenDangNhap);
+        String userName = getIntent().getStringExtra("USERNAME");
+        txtTenDangNhap.setText(userName + "");
 
-    public void ChuyenLogin (View view)
-    {
-        Intent iLogin = new Intent(this, ActivityLogin.class);
-        startActivity(iLogin);
     }
 }
--- IntentLogin/app/src/main/java/com/example/intentlogin/ActivityLogin.java
+++ IntentLogin/app/src/main/java/com/example/intentlogin/ActivityLogin.java
@@ -1,6 +1,12 @@
 package com.example.intentlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ActivityLogin extends AppCompatActivity {
+
+    EditText editUsername, editPass, editEmail;
+    Button btnDangNhap;
+    TextView txtError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,36 @@ public class ActivityLogin extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TimView();
+
+
+    }
+    public void TimView()
+    {
+        editUsername = findViewById(R.id.editUsername);
+        editPass = findViewById(R.id.editPass);
+        editEmail = findViewById(R.id.editEmail);
+        txtError = findViewById(R.id.txtError);
+    }
+
+
+    public void DangNhap (View view)
+    {
+        String userName = editUsername.getText().toString();
+        String pass = editPass.getText().toString();
+
+        if (userName.equals("thonguyenp") && pass.equals("tho123"))
+        {
+            Intent iHome = new Intent(this, ActivityHome.class);
+            iHome.putExtra("USERNAME",userName);
+            startActivity(iHome);
+        }
+        else if (!userName.equals("thonguyenp"))
+            txtError.setText("Sai tên đăng nhập");
+        else if (!pass.equals("tho123")) {
+            txtError.setText("Sai mật khẩu");
+        }
+
+
     }
 }
