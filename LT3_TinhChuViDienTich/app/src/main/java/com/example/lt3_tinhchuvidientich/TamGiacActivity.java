5815640b1a8786b9ae0e0bd0f8e016927a20ddf0
thonguyenp <nphuoctho2406@gmail.com>
2025-02-26 09:16:46 +0700

    Hoàn thành phần backend cho tam giác

--- LT3_TinhChuViDienTich/app/src/main/java/com/example/lt3_tinhchuvidientich/TamGiacActivity.java
+++ LT3_TinhChuViDienTich/app/src/main/java/com/example/lt3_tinhchuvidientich/TamGiacActivity.java
@@ -1,6 +1,10 @@
 package com.example.lt3_tinhchuvidientich;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class TamGiacActivity extends AppCompatActivity {
 
+    EditText editA, editB, editC;
+    Button btnChuVi, btnDienTich;
+    TextView txtResChuVi, txtResDientich;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +27,56 @@ public class TamGiacActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TimView();
     }
+
+    public void TimView()
+    {
+        editA = findViewById(R.id.editA);
+        editB = findViewById(R.id.editB);
+        editC = findViewById(R.id.editC);
+        btnChuVi = findViewById(R.id.btnChuVi);
+        btnDienTich = findViewById(R.id.btnDienTich);
+        txtResChuVi = findViewById(R.id.txtResChuVi);
+        txtResDientich = findViewById(R.id.txtResDienTich);
+    }
+
+    public void TinhChuVi(View v)
+    {
+
+        float a = Float.parseFloat(editA.getText().toString());
+        float b = Float.parseFloat(editB.getText().toString());
+        float c = Float.parseFloat(editC.getText().toString());
+        if ((a + b > c) && (a + c > b) && (b + c > a) &&
+                (a > 0) && (b > 0) && (c > 0))
+        {
+            float cv = a + b + c;
+            txtResChuVi.setText("Chu Vi: " +cv);
+
+        }
+        else
+        {
+            txtResChuVi.setText("Đây không phải là 1 tam giác");
+        }
+    }
+
+    public void TinhDienTich(View v)
+    {
+        float a = Float.parseFloat(editA.getText().toString());
+        float b = Float.parseFloat(editB.getText().toString());
+        float c = Float.parseFloat(editC.getText().toString());
+        if ((a + b > c) && (a + c > b) && (b + c > a) &&
+                (a > 0) && (b > 0) && (c > 0))
+        {
+            float ncv = (a + b + c) / 2;
+            double s = Math.sqrt(ncv * (ncv -a) * (ncv - b) * (ncv - c));
+            txtResDientich.setText("Diện Tích: " +s);
+        }
+        else
+        {
+            txtResDientich.setText("Đây không phải là 1 tam giác");
+        }
+
+    }
+
 }
