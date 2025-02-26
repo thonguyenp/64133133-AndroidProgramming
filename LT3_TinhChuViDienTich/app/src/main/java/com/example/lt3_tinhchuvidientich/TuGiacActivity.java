afb7baa59262c8848723bed39bd3f5f75856dd77
thonguyenp <nphuoctho2406@gmail.com>
2025-02-26 09:31:05 +0700

    Hoàn thành phần backend cho hình chữ nhật

--- LT3_TinhChuViDienTich/app/src/main/java/com/example/lt3_tinhchuvidientich/TuGiacActivity.java
+++ LT3_TinhChuViDienTich/app/src/main/java/com/example/lt3_tinhchuvidientich/TuGiacActivity.java
@@ -1,6 +1,10 @@
 package com.example.lt3_tinhchuvidientich;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class TuGiacActivity extends AppCompatActivity {
 
+    EditText editDai, editRong;
+
+    Button btnChuVi, btnDienTich;
+    TextView txtResChuVi, txtResDienTich;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,34 @@ public class TuGiacActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TimView();
     }
+
+    public void TimView()
+    {
+        editDai = findViewById(R.id.ediDai);
+        editRong = findViewById(R.id.editRong);
+        btnChuVi = findViewById(R.id.btnChuVi);
+        btnDienTich = findViewById(R.id.btnDienTich);
+        txtResChuVi = findViewById(R.id.txtResChuVi);
+        txtResDienTich = findViewById(R.id.txtResDienTich);
+    }
+
+    public void TinhChuVi (View v)
+    {
+        float d = Float.parseFloat(editDai.getText().toString());
+        float r = Float.parseFloat(editRong.getText().toString());
+        float cv = (d + r) * 2;
+        txtResChuVi.setText("Chu Vi: " + cv);
+    }
+
+    public void TinhDienTich (View v)
+    {
+        float d = Float.parseFloat(editDai.getText().toString());
+        float r = Float.parseFloat(editRong.getText().toString());
+        float s = d * r;
+        txtResDienTich.setText("Diện Tích: " + s);
+
+    }
+
 }
