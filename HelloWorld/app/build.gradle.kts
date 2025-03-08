ab5c997792262ac58112336b8fdc363ce8494f8f
thonguyenp <nphuoctho2406@gmail.com>
2025-03-08 07:56:30 +0700

    Chỉnh lại Hello World

--- HelloWorld/app/build.gradle.kts
+++ HelloWorld/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.helloworld"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.helloworld"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
