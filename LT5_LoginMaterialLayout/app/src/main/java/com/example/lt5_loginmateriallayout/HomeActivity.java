0611810f43270bbb4ce32916cf43b0006b3118cf
thonguyenp <nphuoctho2406@gmail.com>
2025-03-12 07:41:37 +0700

    Xong 1/3 backend LT5

---
+++ LT5_LoginMaterialLayout/app/src/main/java/com/example/lt5_loginmateriallayout/HomeActivity.java
@@ -0,0 +1,42 @@
+package com.example.lt5_loginmateriallayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
+
+public class HomeActivity extends AppCompatActivity {
+
+    TextView txtUsername;
+    MaterialButton btnProfile, btnDSHP;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        TimView();
+
+        Intent iHome = getIntent();
+        String username = iHome.getStringExtra("username");
+        txtUsername.setText(username);
+    }
+
+    void TimView ()
+    {
+        txtUsername = findViewById(R.id.txtUsername);
+        btnProfile = findViewById(R.id.btnProfile);
+        btnDSHP = findViewById(R.id.btnDSHP);
+    }
+}
--- LT5_LoginMaterialLayout/app/src/main/java/com/example/lt5_loginmateriallayout/MainActivity.java
+++ LT5_LoginMaterialLayout/app/src/main/java/com/example/lt5_loginmateriallayout/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.lt5_loginmateriallayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
+public class MainActivity extends AppCompatActivity {
+    TextInputEditText editUserName, editPassword;
+    MaterialButton btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +27,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TimView();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editUserName.getText().toString().equalsIgnoreCase("thonguyenp") &&
+                editPassword.getText().toString().equalsIgnoreCase("123"))
+                {
+                    Intent iHome = new Intent(MainActivity.this, HomeActivity.class);
+                    iHome.putExtra("username",editUserName.getText().toString());
+                    startActivity(iHome);
+                }
+                else
+                    Toast.makeText(MainActivity.this, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    void TimView ()
+    {
+        editPassword = findViewById(R.id.editPassword);
+        editUserName = findViewById(R.id.editUsername);
+        btnLogin = findViewById(R.id.btnLogin);
     }
 }
