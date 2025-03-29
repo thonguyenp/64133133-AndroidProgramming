15adb0c4a265ff34a651625eefa63b198fce5e62
thonguyenp <nphuoctho2406@gmail.com>
2025-03-29 14:46:16 +0700

    Xong bài 1 cộng trừ nhân chia

--- TuOnThiGKLan1/app/src/main/java/com/example/tuonthigklan1/MainActivity.java
+++ TuOnThiGKLan1/app/src/main/java/com/example/tuonthigklan1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.tuonthigklan1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnBai1, btnBai2, btnBai3, btnBai4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TimView();
+        btnBai1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ibai1 = new Intent(MainActivity.this, Bai1Activity.class);
+                startActivity(ibai1);
+            }
+        });
+    }
+
+    void TimView ()
+    {
+        btnBai1 = findViewById(R.id.btnBai1);
+        btnBai2 = findViewById(R.id.btnBai2);
+        btnBai3 = findViewById(R.id.btnBai3);
+        btnBai4 = findViewById(R.id.btnBai4);
     }
 }
