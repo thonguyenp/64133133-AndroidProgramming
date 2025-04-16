570b2988077a0b665aafb47a038b534c6a63fca7
thonguyenp <nphuoctho2406@gmail.com>
2025-04-16 10:19:19 +0700

    Tạo project Replace Fragment

---
+++ Ex11_2_THS_Replace_Fragment/app/src/main/java/com/example/ex11_2_ths_replace_fragment/Fragment3.java
@@ -0,0 +1,42 @@
+package com.example.ex11_2_ths_replace_fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Fragment3#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Fragment3 extends Fragment {
+
+    public Fragment3() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Fragment3.
+     */
+    // TODO: Rename and change types and number of parameters
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_3, container, false);
+    }
+}
